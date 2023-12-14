ca8c45061be2d427db856537f35f503defe8b8e7
xasanjon <xasanjonabdunabiyev222@gmail.com>
2023-12-14 22:03:41 +0500

    added

---
+++ js/main.js
@@ -0,0 +1,50 @@
+//
+
+// import { users } from "./users/users";
+
+// let firstName = "xasanjon"
+// let string = `${firstName} frontend debvelopper`;
+// console.log(users);
+
+
+// console.log(users.length);
+
+
+// const str = "       Hello world        ";
+
+
+// console.log(str.trimEnd());
+
+// console.log(str);
+
+
+// const str = "lorem";
+
+// let newStr = "ipsum";
+
+
+// console.log(str.concat(newStr));
+
+// let someThing = "The mirror";
+
+// console.log(someThing.includes("let"));
+
+
+// let text = "The some value this string";
+
+// // console.log(searchingText.includes(" "));
+
+// console.log(text.replace("string", "bor"));
+
+// console.log(text);
+
+
+
+// String creation // string lenght, length, trim(), (trimStart(), trimEnd()), includes(), replace() ToLowerCase(), toUpperCase(), 
+
+
+// CharAt ()
+// indexOf, 
+// slice(), 
+// splice(), 
+// valueOf
---
+++ todo/todo.md
@@ -0,0 +1,9 @@
+JavaScript String Length
+The length property returns the length of a string:
+    1) even a space is a string
+
+
+The trim() method of String values removes whitespace from both ends of this string and returns a new string, without modifying the original string.
+
+Syntax => someString.trim()
+
